855b559cc50511b3450ee9485cc6239eedba421b
lshan <50941420+Lshan523@users.noreply.github.com>
2021-08-11 10:26:05 +0800

    Update AdminServiceApplication.java

--- admin-service/src/main/java/com/sea/admin/AdminServiceApplication.java
+++ admin-service/src/main/java/com/sea/admin/AdminServiceApplication.java
@@ -2,10 +2,9 @@ package com.sea.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class AdminServiceApplication {
